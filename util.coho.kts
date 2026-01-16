9589b2426b404ae63aa2db287770f00bc62b6ba4
Nethul <nethul2011@gmail.com>
2026-01-15 22:51:00 -0800

    More testing!!!

--- util.coho.kts
+++ util.coho.kts
@@ -1,25 +1,34 @@
 fun makeNavBar(data: Any?) = "nav"("class" to "site-nav") {
-    val navBarData = data as List<Map<String, Any?>>
-    for (navItem in navBarData) {
-        if ("children" in navItem) {
-            // Make dropdown
-            "div"("class" to "dropdown") {
-                "button"("class" to "dropbtn") {
-                    append(navItem["title"])
-                }
-                "div"("class" to "dropdown-content") {
-                    for (navChild in navItem["children"] as List<Map<String, Any?>>) {
-                        "a"("href" to navChild["url"]) {
-                            append(navChild["title"].toString())
+    val list = data as? List<*>
+    if (list != null) {
+        for (item in list) {
+            val navItem = item as? Map<*, *> ?: continue
+            
+            if (navItem.containsKey("children")) {
+                // Make dropdown
+                "div"("class" to "dropdown") {
+                    "button"("class" to "dropbtn") {
+                        append(navItem["title"])
+                    }
+                    "div"("class" to "dropdown-content") {
+                        val children = navItem["children"] as? List<*> ?: emptyList<Any>()
+                        for (childItem in children) {
+                            val child = childItem as? Map<*, *> ?: continue
+                            "a"("href" to child["url"]) {
+                                append(child["title"].toString())
+                            }
                         }
                     }
                 }
-            }
-        } else {
-            // Make single
-            "a"("href" to navItem["url"]) {
-                append(navItem["title"])
+            } else {
+                // Make single
+                "a"("href" to navItem["url"]) {
+                    append(navItem["title"])
+                }
             }
         }
+    } else {
+        // Fallback or error indication (optional, or just empty)
+        append("<!-- Navbar data was null or invalid -->")
     }
 }
